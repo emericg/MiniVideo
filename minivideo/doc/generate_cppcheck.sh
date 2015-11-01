0f1f02c771fb63c51114b596a3936fc121217117
Emeric Grange <emeric.grange@gmail.com>
2015-11-01 18:48:52 +0100

    Suppress some cppcheck warnings

--- minivideo/doc/generate_cppcheck.sh
+++ minivideo/doc/generate_cppcheck.sh
@@ -8,7 +8,7 @@ if [[ $(which cppcheck) && $(which cppcheck-htmlreport) ]]; then
             mkdir -p cppcheck_temp
         fi
 
-        cppcheck --enable=all --xml-version=2 ../src/ 2> cppcheck_temp/err.xml
+        cppcheck --enable=all --xml-version=2 --suppressions-list=suppressions.txt ../src/ 2> cppcheck_temp/err.xml
         cppcheck-htmlreport --file=cppcheck_temp/err.xml --report-dir=cppcheck_temp/ --source-dir=../src/
     else
         echo "Error, you must be in the minivideo/doc directory to run this script!"
