4f3ec6d53d67260d5adaf85a2ae68abde23d050d
Emeric Grange <emeric.grange@gmail.com>
2015-11-23 20:41:38 +0100

    Add an MP3 parser skeleton

---
+++ minivideo/src/demuxer/mp3/mp3.c
@@ -0,0 +1,49 @@
+/*!
+ * COPYRIGHT (C) 2015 Emeric Grange - All Rights Reserved
+ *
+ * This file is part of MiniVideo.
+ *
+ * MiniVideo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MiniVideo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MiniVideo.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * \file      mp3.c
+ * \author    Emeric Grange <emeric.grange@gmail.com>
+ * \date      2015
+ */
+
+// minivideo headers
+#include "mp3.h"
+#include "mp3_struct.h"
+#include "../../bitstream.h"
+#include "../../bitstream_utils.h"
+#include "../../typedef.h"
+#include "../../minitraces.h"
+
+// C standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/* ************************************************************************** */
+
+int mp3_fileParse(VideoFile_t *video)
+{
+    int retcode = FAILURE;
+
+    TRACE_INFO(MP3, BLD_GREEN "mp3_fileParse()\n" CLR_RESET);
+
+    return retcode;
+}
+
+/* ************************************************************************** */
---
+++ minivideo/src/demuxer/mp3/mp3_struct.h
@@ -0,0 +1,33 @@
+/*!
+ * COPYRIGHT (C) 2015 Emeric Grange - All Rights Reserved
+ *
+ * This file is part of MiniVideo.
+ *
+ * MiniVideo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MiniVideo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MiniVideo.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * \file      mp3_struct.h
+ * \author    Emeric Grange <emeric.grange@gmail.com>
+ * \date      2015
+ */
+
+#ifndef PARSER_MP3_STRUCT_H
+#define PARSER_MP3_STRUCT_H
+
+// minivideo headers
+#include "../../typedef.h"
+
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+#endif // PARSER_MP3_STRUCT_H
