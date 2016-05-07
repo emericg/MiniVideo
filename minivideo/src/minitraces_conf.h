784b3e42ed459f907b14121b3c4e20c4c7f37a32
Emeric Grange <emeric.grange@gmail.com>
2016-05-07 16:57:06 +0200

    minivideo: update minitraces, fix timestamping on osx

--- minivideo/src/minitraces_conf.h
+++ minivideo/src/minitraces_conf.h
@@ -19,7 +19,7 @@
  * \file      minitraces_conf.h
  * \author    Emeric Grange <emeric.grange@gmail.com>
  * \date      2015
- * \version   0.43
+ * \version   0.44
  */
 
 #ifndef MINITRACES_CONF_H
