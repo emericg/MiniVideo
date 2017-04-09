ee8fd22497d7d1c9579e2349af6997561c2e6a5f
Emeric Grange <emeric.grange@gmail.com>
2017-04-09 12:15:30 +0200

    minivdeo: Add a method to autodetect linux kernel version and memfd support with cmake

---
+++ minivideo/cmake/modules/FindMemFD.cmake
@@ -0,0 +1,37 @@
+# - Find linux kernel version and memfd availability.
+#
+# This module defines:
+#  MEMFD_FOUND, TRUE if we have found a linux kernel version >= 3.17.
+#  KERNEL_RELEASE (ex: 4.10.8-1-ARCH)
+#  KERNEL_VERSION (ex: 410)
+#  KERNEL_VERSION_MAJOR (ex: 4)
+#  KERNEL_VERSION_MINOR (ex: 10)
+
+set(MEMFD_FOUND FALSE)
+
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+
+    execute_process(COMMAND uname -r
+                    OUTPUT_VARIABLE KERNEL_RELEASE
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    execute_process(COMMAND uname -r
+                    COMMAND cut -d. -f1-2 --output-delimiter=
+                    OUTPUT_VARIABLE KERNEL_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    execute_process(COMMAND uname -r
+                    COMMAND cut -c 3-4
+                    OUTPUT_VARIABLE KERNEL_VERSION_MINOR
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    execute_process(COMMAND uname -r
+                    COMMAND cut -c 1-1
+                    OUTPUT_VARIABLE KERNEL_VERSION_MAJOR
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    if(${KERNEL_VERSION} GREATER_EQUAL 317)
+        set(MEMFD_FOUND TRUE)
+    endif()
+
+endif()
